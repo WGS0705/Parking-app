451e857e7011b508a950cab5fb21e6649c4616f5
WGS0705 <Lucnovrod1@alum.us.es>
2025-11-24 12:16:22 +0100

    Primer commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const imgPsico = document.querySelector('a[href = "index_psicologia.html"]');
+const imgEtsii = document.querySelector('a[href = "index_etsii.html"]');
+
+imgPsico.addEventListener('click',function(event){
+    alert("Bienvenido al parking de Psicologia")
+});
+
+imgEtsii.addEventListener('click',function(event){
+    alert("Bienvenido al parking de la ETSII")
+});
